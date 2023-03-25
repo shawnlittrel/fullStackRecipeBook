c8eeb9d3717e798b5766262b34fea14760b2ba3d
Shawn Littrel <shawnlittrel@protonmail.com>
2023-03-25 12:57:23 -0700

    changing to svelte

---
+++ src/routes/someNewContent.svelte
@@ -0,0 +1,15 @@
+<script>
+  function runButton() {
+    event?.preventDefault();
+    console.log('button click');
+  }
+</script>
+
+<div>
+  Here's some additional content that was imported
+  <button on:click={runButton}>Click Me</button>
+</div>
+
+<style>
+
+</style>
